604db7a723e31ee97f5f11c7e17caf2b7c07c200
Serraniel <mail@serraniel.dev>
2020-04-25 22:43:36 +0200

    #1 Change border color of own requests

--- enhancements/animeRequests.js
+++ enhancements/animeRequests.js
@@ -1,6 +1,7 @@
 const starIcon = "star";
 
 executeAfterPreload(changeFollowedStarColor);
+executeAfterPreload(changeOwnBorderColor);
 
 function changeFollowedStarColor() {
     // find stars
@@ -8,4 +9,18 @@ function changeFollowedStarColor() {
 
     // change color
     followedItems.forEach(item => item.style.color = aniBlue);
+}
+
+function changeOwnBorderColor() {
+    // find items -> all 
+    let requestItems = document.querySelectorAll("md-list-item");
+
+    // change border color if profile link is not "false"
+    requestItems.forEach(item => {
+        let profileLink = item.querySelectorAll("a[href*='/profile/']:not([href='/profile/false'])");
+
+        if (profileLink.length > 0) {
+            item.style.borderColor = aniBlue
+        }
+    });
 }
