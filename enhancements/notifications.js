1715e8fa7f6bda9f22b90c5630e6c4d32fd3e257
kaffem <29717789+kaffem@users.noreply.github.com>
2020-07-29 23:09:13 +0200

    using space indentation instead of tab

--- enhancements/notifications.js
+++ enhancements/notifications.js
@@ -3,8 +3,8 @@ runAfterLoad(() => {
 }, ".*");
 
 function displayNotificationsInTitle(){
-let menu = document.getElementById('materialize-menu-dropdown');
-let menuDropdowns = Array.from(menu.querySelectorAll('ul.dropdown')).slice(-1)[0];
-let notificationText = menuDropdowns.innerText.split("  ")[3];
-document.querySelector('head > title').insertAdjacentText('afterbegin', notificationText.split(" ")[1] + ' ');
+    let menu = document.getElementById('materialize-menu-dropdown');
+    let menuDropdowns = Array.from(menu.querySelectorAll('ul.dropdown')).slice(-1)[0];
+    let notificationText = menuDropdowns.innerText.split("  ")[3];
+    document.querySelector('head > title').insertAdjacentText('afterbegin', notificationText.split(" ")[1] + ' ');
 }
