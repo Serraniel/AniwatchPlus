150d6bcfbb61c7a33f02ecba896c60b8878b5211
kaffem <29717789+kaffem@users.noreply.github.com>
2020-08-04 17:42:31 +0200

    #19 changing the title on page navigation

--- enhancements/notifications.js
+++ enhancements/notifications.js
@@ -1,10 +1,21 @@
+let __notificationCount = '';
+
 runAfterLoad(() => {
+    __notificationCount = getNotificationCount();
     displayNotificationsInTitle();
 }, ".*");
 
-function displayNotificationsInTitle(){
+runAfterPathnameChange(() => {
+    displayNotificationsInTitle();
+}, ".*");
+
+function getNotificationCount() {
     let menu = document.getElementById('materialize-menu-dropdown');
     let menuDropdowns = Array.from(menu.querySelectorAll('ul.dropdown')).slice(-1)[0];
     let notificationText = menuDropdowns.innerText.split("  ")[3];
-    document.title = notificationText.split(" ")[1] + ' ' + document.title;
+    return notificationText.split(" ")[1] + ' ';
+}
+
+function displayNotificationsInTitle(){
+    document.title = __notificationCount + document.title;
 }
--- utils/aniwatchCore.js
+++ utils/aniwatchCore.js
@@ -1,5 +1,7 @@
 let __scripts = [];
 let __afterLoadScripts = [];
+let __afterPopstateScripts = [];
+let __afterPathnameChangeScripts = [];
 
 function registerScript(func, pattern = '.*') {
     __scripts.push({ "function": func, "pattern": pattern });
@@ -45,6 +47,37 @@ function awaitPageLoaded() {
             clearInterval(loop);
 
             __afterLoadScripts.forEach(script => {
+                if (window.location.pathname.match(script.pattern)) {
+                    script.function();
+                }
+            })
+        }
+    }, 100);
+}
+
+function runAfterPathnameChange(func, pattern = '.*') {
+    __afterPathnameChangeScripts.push({ "function": func, "pattern": pattern});
+}
+
+let locationPath = location.pathname;
+let __loop = setInterval(() => {
+    if (locationPath != location.pathname) {
+        locationPath = location.pathname;
+        awaitPathnameChange();
+    }
+}, 100);
+
+function awaitPathnameChange() {
+    let preLoader = document.getElementById('preloader');
+
+    if (typeof preLoader === 'undefined') {
+        return;
+    }
+
+    let loop = setInterval(() => {
+        if (preLoader.style.display === "none") {
+            clearInterval(loop);
+            __afterPathnameChangeScripts.forEach(script => {
                 if (window.location.pathname.match(script.pattern)) {
                     script.function();
                 }
