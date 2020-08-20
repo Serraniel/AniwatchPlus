a6654ebbf760eb99ce01a425451c7348056146c3
kaffem <29717789+kaffem@users.noreply.github.com>
2020-08-20 16:09:08 +0200

    simplified retrieving the notification count

--- enhancements/notifications.js
+++ enhancements/notifications.js
@@ -10,10 +10,8 @@ runAfterPathnameChange(() => {
 }, ".*");
 
 function getNotificationCount() {
-    let menu = document.getElementById('materialize-menu-dropdown');
-    let menuDropdowns = Array.from(menu.querySelectorAll('ul.dropdown')).slice(-1)[0];
-    let notificationText = menuDropdowns.innerText.split("  ")[3];
-    return notificationText.split(" ")[1] + ' ';
+    let menuUserText = document.getElementById('materialize-menu-dropdown').innerText.split('\n')[4];
+    return menuUserText.split(" ")[1] + ' ';
 }
 
 function displayNotificationsInTitle(){
