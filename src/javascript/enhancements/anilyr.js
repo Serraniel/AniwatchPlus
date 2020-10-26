f8b0dbbe3fc45937d22e7faedf2fd283365cb07a
Serraniel <mail@serraniel.dev>
2020-10-26 20:42:34 +0100

    #94 Added empty observer for the player

--- src/javascript/enhancements/anilyr.js
+++ src/javascript/enhancements/anilyr.js
@@ -1,10 +1,26 @@
 import * as core from '../utils/aniwatchCore';
 import * as helper from '../utils/helpers';
 
+const SCREENSHOT_TOOLTIP_ID = 'anilyr-screenshots-tooltip';
+
 export function init() {
     core.registerScript(node => {
-        // run the scripts
-       
+        if (helper.isHtmlElement(node) && node.id === SCREENSHOT_TOOLTIP_ID) {
+            observeScreenshotTooltip(node);
+        }
     }, "^/anime/[0-9]*/[0-9]*$");
 }
 
+function observeScreenshotTooltip(tooltip) {
+    let observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            
+        });
+    });
+
+    observer.observe(tooltip, {
+        attributes: true,
+        attributeOldValue: true,
+        attributeFilter: ['style'],        
+    });
+}
