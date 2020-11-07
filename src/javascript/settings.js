88a97af8f47e369de7541862544308beb23864ab
Serraniel <mail@serraniel.dev>
2020-11-07 16:47:31 +0100

    #32 Probably working script for a settings page?

--- src/javascript/settings.js
+++ src/javascript/settings.js
@@ -1,11 +1,28 @@
+import { getGlobalStorageProvider } from "./settings/storageProvider";
 import { onReady } from "./utils/helpers";
 
-onReady(() => {
+const OPTION_SELECTOR = '.extension-option'
 
-});
-
-function saveOptions() {
-
+function storeOptions() {
+    document.querySelectorAll(OPTION_SELECTOR).forEach(optionElement => {
+        getGlobalStorageProvider().setData(optionElement.id, optionElement.check);
+    });
 }
 
-document.getElementById('save').addEventListener('click', saveOptions)
+function restoreOptions() {
+    document.querySelectorAll(OPTION_SELECTOR).forEach(optionElement => {
+        let defaultValue = optionElement.dataset.defaultValue === 'true' ? true : false;
+
+        getGlobalStorageProvider().getData(optionElement.id, defaultValue, value => {
+            optionElement.checked = value;
+        });
+    });
+}
+
+onReady(() => {
+    // register Store Button
+    document.getElementById('saveBtn').addEventListener('click', storeOptions);
+
+    // try restore options
+    restoreOptions();
+});
