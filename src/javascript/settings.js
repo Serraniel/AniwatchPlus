f5c5af92ca735a47510a9d7e92c286f4a3593062
Serraniel <mail@serraniel.dev>
2020-11-07 17:02:24 +0100

    #32 Added reset Button to options page and fixed settings script

--- src/javascript/settings.js
+++ src/javascript/settings.js
@@ -1,11 +1,11 @@
 import { getGlobalStorageProvider } from "./settings/storageProvider";
 import { onReady } from "./utils/helpers";
 
-const OPTION_SELECTOR = '.extension-option'
+const OPTION_SELECTOR = 'input[type="checkbox"';
 
 function storeOptions() {
     document.querySelectorAll(OPTION_SELECTOR).forEach(optionElement => {
-        getGlobalStorageProvider().setData(optionElement.id, optionElement.check);
+        getGlobalStorageProvider().setData(optionElement.id, optionElement.checked);
     });
 }
 
@@ -19,9 +19,26 @@ function restoreOptions() {
     });
 }
 
+function resetOptions() {
+    document.querySelectorAll(OPTION_SELECTOR).forEach(optionElement => {
+        let defaultValue = optionElement.dataset.defaultValue === 'true' ? true : false;
+
+        optionElement.checked = defaultValue;
+    });
+}
+
 onReady(() => {
     // register Store Button
-    document.getElementById('saveBtn').addEventListener('click', storeOptions);
+    document.getElementById('btnSave').addEventListener('click', event => {
+        event.preventDefault();
+        storeOptions();
+    });
+
+    document.getElementById('btnReset').addEventListener('click', event => {
+        event.preventDefault();
+        resetOptions();
+        storeOptions();
+    })
 
     // try restore options
     restoreOptions();
